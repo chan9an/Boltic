d1bd4ca5ec81a51e234f92806e8fa429be97051d
chan9an <devchandansr@gmail.com>
2025-12-12 12:12:20 +0530

    Enable CORS

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const PORT = process.env.PORT || 3000;
 // ===============================
 //    MIDDLEWARE
 // ===============================
+app.use(cors());
 app.use(express.json());
 app.use(cors({ origin: "*" }));
 app.use(express.static(path.join(__dirname, "public")));
